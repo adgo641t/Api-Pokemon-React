b6f96f2468db257536aab82f62a914dc0491ad2d
adrian <fpb1.adrian@gmail.com>
2024-12-17 17:18:45 +0100

    API

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Api_pokemon } from './Api_pokemon'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Api_pokemon />
+    </>
+  )
+}
+
+export default App
